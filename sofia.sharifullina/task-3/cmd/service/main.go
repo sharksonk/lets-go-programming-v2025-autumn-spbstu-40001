498b4fe60cc1fb24f94efe48757aacbe7aab1e10
sharksonk <sharksonk@gmail.com>
2025-10-31 04:05:32 +0300

    [TASK-3] add structures and xml parsing

--- sofia.sharifullina/task-3/cmd/service/main.go
+++ sofia.sharifullina/task-3/cmd/service/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"encoding/xml"
+	"flag"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 
 	"gopkg.in/yaml.v3"
 )
@@ -10,6 +14,37 @@ import (
 type Config struct {
 	InputFile  string `yaml:"input-file"`
 	OutputFile string `yaml:"output-file"`
+}
+
+type CurrencyValue float64
+
+type Valute struct {
+	NumCode  int           `json:"num_code" xml:"NumCode"`
+	CharCode string        `json:"char_code" xml:"CharCode"`
+	Value    CurrencyValue `json:"value" xml:"Value"`
+}
+type ValCurs struct {
+	XMLName xml.Name `xml:"ValCurs"`
+	Valutes []Valute `xml:"Valute"`
+}
+
+func (cv *CurrencyValue) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var str string
+
+	err := d.DecodeElement(&str, &start)
+	if err != nil {
+		return fmt.Errorf("failed to decode currency value: %w", err)
+	}
+
+	str = strings.ReplaceAll(str, ",", ".")
+
+	value, err := strconv.ParseFloat(str, 64)
+	if err != nil {
+		return fmt.Errorf("failed to parse currency value '%s': %w", str, err)
+	}
+
+	*cv = CurrencyValue(value)
+	return nil
 }
 
 func readConfig(configPath string) (*Config, error) {
@@ -28,4 +63,39 @@ func readConfig(configPath string) (*Config, error) {
 	return &config, nil
 }
 
-func main() {}
+func parseXML(path string) ([]Valute, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read XML file: %w", err)
+	}
+
+    var valCurs ValCurs
+	err = xml.Unmarshal(data, &valCurs)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse XML: %w", err)
+	}
+
+	return valCurs.Valutes, nil
+}
+
+func saveJSON(path string, valutes []Valute) error {}
+
+func main() {
+	configPath := flag.String("config", "configs/config.yaml", "path to config file")
+	flag.Parse()
+
+	config, err := readConfig(*configPath)
+	if err != nil {
+		panic(err)
+	}
+
+	valutes, err := parseXML(config.InputFile)
+	if err != nil {
+		panic(err)
+	}
+
+	err = saveJSON(config.OutputFile, valutes)
+	if err != nil {
+		panic(err)
+	}
+}
