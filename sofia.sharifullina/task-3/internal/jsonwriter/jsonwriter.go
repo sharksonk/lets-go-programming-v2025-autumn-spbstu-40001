0036787950411790a94ad50f541ab59687def89e
sharksonk <sharksonk@gmail.com>
2026-01-09 01:24:54 +0300

    [TASK-3] prosto poprabila

--- sofia.sharifullina/task-3/internal/jsonwriter/jsonwriter.go
+++ sofia.sharifullina/task-3/internal/jsonwriter/jsonwriter.go
@@ -7,15 +7,15 @@ import (
 	"path/filepath"
 )
 
-func SaveJSON(outputFile string, data any,  filePermission, dirPermission os.FileMode) error {
+func SaveJSON(outputFile string, data any, filePermission, dirPermission os.FileMode) error {
 	dir := filepath.Dir(outputFile)
 
-	err := os.MkdirAll(dir, permission)
+	err := os.MkdirAll(dir, dirPermission)
 	if err != nil {
 		return fmt.Errorf("failed to creating directory: %w", err)
 	}
 
-	file, err := os.Create(outputFile)
+	file, err := os.Create(outputFile, os.O_RDWR|os.O_CREATE|os.O_TRUNC, filePermission)
 	if err != nil {
 		return fmt.Errorf("failed to creating file: %w", err)
 	}
@@ -24,7 +24,7 @@ func SaveJSON(outputFile string, data any,  filePermission, dirPermission os.Fil
 		err = file.Close()
 		if err != nil {
 			panic("failed to close file in saveJSON")
-                }
+		}
 	}()
 
 	encoder := json.NewEncoder(file)
